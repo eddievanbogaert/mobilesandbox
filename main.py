849be6e3597f3044f6d3ade7d2e7d8e14a514bda
Eddie VanBogaert <35679282+eddievanbogaert@users.noreply.github.com>
2023-06-20 03:37:21 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-import datetime
+from flask import Flask
 
-from flask import Flask, render_template
+# Import the handlers defined in the Blueprint for myapp1_handlers
+from myapp1 import myapp1_handlers 
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [
-        datetime.datetime(2018, 1, 1, 10, 0, 0),
-        datetime.datetime(2018, 1, 2, 10, 30, 0),
-        datetime.datetime(2018, 1, 3, 11, 0, 0),
-    ]
-
-    return render_template("index.html", times=dummy_times)
+# Register your blueprint so that any route matching 
+# what you've defined in the blueprint will get routed to it
+app.register_blueprint(myapp1_handlers)
 
 
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+@app.route("/") 
+def home():
+    return "Main App - This is home page"
+
+
+@app.route("/1/") 
+def home_1():
+    return "Main App - This is page 1"
